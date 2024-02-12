0f1ddafec7e17f3607f319132e93fda902208370
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-12 16:52:53 +0100

    Extract navBar routes to an array

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -1,3 +1,15 @@
+@php
+    
+    $links = [
+        [
+            'name' => 'Dashboard',
+            'url' => route('dashboard'),
+            'active' => request()->routeIs('dashboard')
+        ],
+    ];
+
+@endphp
+
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,9 +24,11 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+                    @foreach ($links as $link)
+                        <x-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                            {{ $link['name'] }}
+                        </x-nav-link>
+                    @endforeach
                 </div>
             </div>
 
@@ -150,9 +164,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
+            @foreach ($links as $link)
+                <x-responsive-nav-link href="{{ $link['url'] }}" :active="$link['active']">
+                    {{ $link['name'] }}
+                </x-responsive-nav-link>
+            @endforeach            
 
             @guest 
                 <x-responsive-nav-link href="{{ route('login') }}" :active="request()->routeIs('login')">
@@ -162,7 +178,7 @@
                 <x-responsive-nav-link href="{{ route('register') }}" :active="request()->routeIs('register')">
                     Register
                 </x-responsive-nav-link>
-            @endguest
+            @endguest 
         </div>
 
         @auth
