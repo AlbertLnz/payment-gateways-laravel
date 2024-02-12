899fb8ef34f2f735da9703a56eac7f307077e607
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-12 17:01:38 +0100

    Delete '/dashboard' route for '/' homepage

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,9 +2,9 @@
     
     $links = [
         [
-            'name' => 'Dashboard',
-            'url' => route('dashboard'),
-            'active' => request()->routeIs('dashboard')
+            'name' => 'HomePage',
+            'url' => '/',
+            'active' => false
         ],
     ];
 
@@ -17,7 +17,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="/">
                         <x-application-mark class="block h-9 w-auto" />
                     </a>
                 </div>
