d96932b3f0ed511a573c1ed27af338723e6e745c
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-13 19:20:24 +0100

    Subscriptions on all plans and Spinner on Enterprise plan

--- app/Livewire/Subscription.php
+++ app/Livewire/Subscription.php
@@ -6,10 +6,22 @@ use Livewire\Component;
 
 class Subscription extends Component
 {
-    public function newSubscription($plan) {
 
-        dd($plan);
+    // Computed property
+    public function getDefaultPaymentMethodProperty() {
+        return auth()->user()->defaultPaymentMethod();
+    }
 
+    public function newSubscription($planId) {
+
+        // dd($planId);
+
+        auth()->user()->newSubscription('Suscripciones blog', $planId)
+            ->create($this->defaultPaymentMethod->id); 
+        
+        // EXPLICATIONS:
+        // newSubscription('nameOfProduct', 'planId') <- If planId it's not inserted, it configure default plan from Stripe
+        // create($this->defaultPaymentMethod->id) <---- Card selected will be the default card ('Predeterminado')
     }
 
     public function render()
