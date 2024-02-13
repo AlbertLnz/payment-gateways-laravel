551382708a1facfc0b4107a262d5f3880b488bef
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-13 12:12:20 +0100

    Stripe confirmCardSetup method - test mode

--- app/Livewire/PaymentMethods.php
+++ app/Livewire/PaymentMethods.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class PaymentMethods extends Component
 {
+    
+    public function addPaymentMethod($paymentMethod) {
+
+        auth()->user()->addPaymentMethod($paymentMethod);
+
+    }
+
     public function render()
     {
         return view('livewire.payment-methods', [
--- resources/views/livewire/payment-methods.blade.php
+++ resources/views/livewire/payment-methods.blade.php
@@ -9,11 +9,13 @@
 
                 <p class="text-gray-600 mr-6">Información de tarjeta</p>
 
-                <div class="flex-1">
+                <div class="flex-1" wire:ignore>
                     <input id="card-holder-name" class="form-control mb-4" placeholder="Nombre del titular de la tarjeta">
  
                     <!-- Stripe Elements Placeholder -->
                     <div id="card-element" class="form-control"></div>
+
+                    <span id="card-error-message" class="text-red-600 text-sm mt-2"></span>
                 </div>
 
             </div>
@@ -41,13 +43,45 @@
         <script src="https://js.stripe.com/v3/"></script>
     
         <script>
-            const stripe = Stripe('stripe-public-key');
+            const stripe = Stripe("{{ env('STRIPE_KEY') }}");
         
             const elements = stripe.elements();
             const cardElement = elements.create('card');
         
             cardElement.mount('#card-element');
         </script>
+
+        <script>
+            const cardHolderName = document.getElementById('card-holder-name');
+            const cardButton = document.getElementById('card-button');
+            const clientSecret = cardButton.dataset.secret;
+            
+            cardButton.addEventListener('click', async (e) => {
+                const { setupIntent, error } = await stripe.confirmCardSetup(
+                    clientSecret, {
+                        payment_method: {
+                            card: cardElement,
+                            billing_details: { name: cardHolderName.value }
+                        }
+                    }
+                );
+            
+                if (error) {
+                    // Display "error.message" to the user...
+
+                    // console.log(error.message)
+                    let span = document.getElementById('card-error-message')
+                    span.textContent = error.message
+
+                } else {
+                    // The card has been verified successfully...
+
+                    console.log(setupIntent.payment_method)
+                    @this.addPaymentMethod(setupIntent.payment_method)
+                }
+            });
+        </script>
+
     @endpush
 
 </div>
