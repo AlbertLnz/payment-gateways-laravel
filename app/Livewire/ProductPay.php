477ebbba03f710a40f81c79c82291049656f1799
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-14 18:13:09 +0100

    Set up a default payment w/ Badge using Computed property

--- app/Livewire/ProductPay.php
+++ app/Livewire/ProductPay.php
@@ -10,6 +10,15 @@ class ProductPay extends Component
     public $product;
     public $paymentMethodSelected;
 
+    public function __construct() {
+        $this->paymentMethodSelected = $this->getDefaultPaymentMethodProperty()->id; // At started, the value of $paymentMethodSelected will be the default payment method
+    }
+
+    // Computed property
+    public function getDefaultPaymentMethodProperty() {
+        return auth()->user()->defaultPaymentMethod();
+    }
+
     public function render()
     {
         return view('livewire.product-pay', [
