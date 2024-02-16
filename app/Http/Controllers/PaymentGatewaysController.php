2132ade2174d1869643e9fb0d28d195a1c9e44e2
AlbertLnz <albert2000_lanza@hotmail.com>
2024-02-16 10:06:46 +0100

    Niubiz Session Token generated

--- app/Http/Controllers/PaymentGatewaysController.php
+++ app/Http/Controllers/PaymentGatewaysController.php
@@ -14,7 +14,10 @@ class PaymentGatewaysController extends Controller
         // IziPay
         $iziPay_formToken = $this->izipay_generateFormToken();
 
-        return view('gateways.index', compact('iziPay_formToken'));
+        // Niubiz
+        $niubiz_sessionToken = $this->niubiz_generateSessionToken();
+
+        return view('gateways.index', compact('iziPay_formToken', 'niubiz_sessionToken'));
     }
 
     private function izipay_generateFormToken() {
@@ -47,7 +50,7 @@ class PaymentGatewaysController extends Controller
 
     private function niubiz_generateAccessToken() {
 
-        $url = config('services.niubiz.url_api');
+        $url = config('services.niubiz.url_api') . '/api.security/v1/security';
         $user = config('services.niubiz.user');
         $password = config('services.niubiz.password');
         
@@ -60,5 +63,42 @@ class PaymentGatewaysController extends Controller
         $response = Http::withHeaders($header)->get($url)->body();
 
         return $response;
+    }
+    private function niubiz_generateSessionToken() {
+        
+        $accessToken = $this->niubiz_generateAccessToken();
+
+        $merchantId = config('services.niubiz.merchant_id');
+
+        $url = config('services.niubiz.url_api') . "/api.ecommerce/v2/ecommerce/token/session/$merchantId";
+
+        $headers = [
+            'Authorization' => $accessToken,
+            'Content-Type' => 'application/json'
+        ];
+
+        $body = [
+            "channel" => 'web',
+            "amount" => 100, // in dolars (accepts 2 decimals: 100.87)
+            "antifraud	" =>  [
+                'clientIp' => request()->ip(),
+                'merchantDefineData' => [
+                    'MDD4'=> auth()->user()->email, // client email
+                    'MDD21' => 0, // 1: frequent client ; 0: No frequent client -> depen on your website
+                    'MDD32' => auth()->id(), // unique id
+                    'MDD75' => 'Registrado', // User is: Registrado, Invitado o Empleado
+                    'MDD77' => now()->diffInDays(auth()->user()->created_at) + 1, // how many days when user registered to your website. Minim value must be at least 1!
+                ]
+            ],
+            "customer" => [
+                "email" => auth()->user()->email,
+            ]
+        ];
+
+        $response = Http::withHeaders($headers)->post($url, $body)->json();
+
+        //$response -> Array['sessionKey', 'expirationTime']
+
+        return $response['sessionKey'];
     }
 }
